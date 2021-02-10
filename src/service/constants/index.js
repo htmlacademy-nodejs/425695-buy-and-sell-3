3468200f9b2008b1f0795de63dc5478f7497cbd9
dimitriskot <mopo_kdi-web@mail.ru>
2021-02-10 13:02:47 +0300

    module2-task2

--- src/service/constants/index.js
+++ src/service/constants/index.js
@@ -8,45 +8,6 @@ const ExitCode = {
 };
 const FILE_NAME = `mocks.json`;
 
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-  `Продам коллекцию журналов «Огонёк»`,
-  `Отдам в хорошие руки подшивку «Мурзилка»`,
-  `Продам советскую посуду. Почти не разбита`,
-  `Куплю детские санки`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `Две страницы заляпаны свежим кофе.`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-  `Кажется, что это хрупкая вещь.`,
-  `Мой дед не мог её сломать.`,
-  `Кому нужен этот новый телефон, если тут такое...`,
-  `Не пытайтесь торговаться. Цену вещам я знаю.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
-
 const OfferType = {
   OFFER: `offer`,
   SALE: `sale`,
@@ -65,16 +26,20 @@ const PictureRestrict = {
 
 const USER_ARGV_INDEX = 2;
 
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+
 module.exports = {
-  CATEGORIES,
   DEFAULT_COMMAND,
   DEFAULT_COUNT,
   ExitCode,
+  FILE_CATEGORIES_PATH,
   FILE_NAME,
+  FILE_SENTENCES_PATH,
+  FILE_TITLES_PATH,
   OfferType,
   PictureRestrict,
-  SENTENCES,
   SumRestrict,
-  TITLES,
   USER_ARGV_INDEX,
 };
